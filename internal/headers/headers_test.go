a39949fd786b0a777e4d0a7047a9938ed09fdb6d
Aayushman-nvm <aayushman.jha001@gmail.com>
2026-06-08 03:05:08 +0530

    ch 5

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,46 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Test: Valid single header
+func TestHeaderParse(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFoo:     bar\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "bar", headers.Get("Foo"))
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, 37, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// test: invalid character in header key
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069,localhost:42069", headers.Get("HOST"))
+	assert.False(t, done)
+}
